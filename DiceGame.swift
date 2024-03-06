85a2f7005994084e6b9bc851e40e2c9cd1957620
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-03-06 00:09:39 +0000

    Initial commit

--- DiceGame.swift
+++ DiceGame.swift
@@ -1,4 +1,3 @@
-
 //  DiceGame.swift
 //
 //  Created by Christopher Di Bert
@@ -6,4 +5,47 @@
 //  Version 1.0
 //  Copyright (c) Christopher Di Bert. All rights reserved.
 //
-//  Change me.
+//  Guess the correct dice value to win!
+
+
+func main() {
+    // Generates a random number from 1-6.
+    let randomNum: Int = Int.random(in:1..<7)
+    // Initializes the number of guesses.
+    var guesses: Int = 0
+    // Loop repeats until the user guesses correctly.
+    repeat {
+        // Increments the number of guesses by 1.
+        guesses += 1
+        // Gets the user's guess and checks for input errors.
+        print("Enter a guess (1-6): ", terminator: "")
+        if let userGuessStr = readLine(), let userGuess = Int(userGuessStr) {
+            // Checks if the user's guess is in range.
+            if userGuess >= 1 && userGuess <= 6 {
+                if userGuess < randomNum {
+                    print("Too small!")
+                }
+                else if userGuess > randomNum {
+                    print("Too large!")
+                }
+                // If the user guessed correctly.
+                else {
+                    break
+                }
+            }
+            // If the user entered a number beyond 1-6.
+            else {
+                print("You must enter a number from (1-6)")
+                guesses -= 1
+            }
+        }
+        // If the user did not enter an integer.
+        else {
+            print("You must enter an integer!")
+            guesses -= 1
+        }
+    } while true
+    // Once the user guesses correctly, tell them how many guesses it took.
+    print("Correct! It took you \(guesses) to guess correctly!")
+}
+main()
